8098dabbdf28d723200c91ba8be8fb5cf5c3f0e2
Dalton Natale <cdalton.natale@gmail.com>
2020-10-19 10:34:51 -0700

    remove react act logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
